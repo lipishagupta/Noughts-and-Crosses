b318a1a068c1759eda7b92e46880fe1112d05acf
Devansh Kaushik <61897385+dkdevansh@users.noreply.github.com>
2020-11-21 13:07:03 +0530

    Add files via upload

---
+++ TIC-TAC-TOE.py
@@ -0,0 +1,52 @@
+Python 3.8.5 (tags/v3.8.5:580fbb0, Jul 20 2020, 15:43:08) [MSC v.1926 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def GAME():	
+	P1="p1"
+	P2="p2"
+	board=['','','','','','','','','']
+	for i in range(9):
+	N=input("enter the patner name")
+	if N==P1:
+	Player1=int(input("PLAYER 1ENTER THE NUMBER AT WHICH YOU WANT TO PLACE:"))
+	board.pop(Player1)
+	board.insert(Player1,"X")
+	print(board[0],' | ',board[1],' | ',board[2],end='\n')
+	print('--------------------------',end='\n')
+	print(board[3],' | ',board[4],' | ',board[5],end='\n')
+	print('--------------------------',end='\n')
+	print(board[6],' | ',board[7],' | ',board[8],end='\n')
+	if ((board[0] and board[1] and board[2]) or (board[3] and board[4] and board[5]) or (board[6] and board[7] and board[8]) or
+	(board[2] and board[1] and board[0]) or (board[5] and board[4] and board[3]) or (board[8] and board[7] and board[6])or
+	(board[0] and board[3] and board[6]) or (board[1] and board[4] and board[7]) or (board[2] and board[5] and board[8]) or
+	(board[6] and board[3] and board[0]) or (board[7] and board[4] and board[1]) or (board[8] and board[7] and board[2])or
+	(board[0] and board[4] and board[8]) or (board[2] and board[4] and board[6])or
+	(board[8] and board[4] and board[0]) or (board[6] and board[4] and board[2])):
+	break
+	if N==P2:
+	Player2=int(input("PLAYER2 ENTER THE NUMBER AT WHICH YOU WANT TO PLACE:"))
+	board.pop(Player2)
+	board.insert(Player2,"O")
+	print(board[0],' | ',board[1],' | ',board[2],end='\n')
+	print('--------------------------',end='\n')
+	print(board[3],' | ',board[4],' | ',board[5],end='\n')
+	print('--------------------------',end='\n')
+	print(board[6],' | ',board[7],' | ',board[8],end='\n')
+	if ((board[0] and board[1] and board[2]) or (board[3] and board[4] and board[5]) or (board[6] and board[7] and board[8]) or
+	(board[2] and board[1] and board[0]) or (board[5] and board[4] and board[3]) or (board[8] and board[7] and board[6])or
+	(board[0] and board[3] and board[6]) or (board[1] and board[4] and board[7]) or (board[2] and board[5] and board[8]) or
+	(board[6] and board[3] and board[0]) or (board[7] and board[4] and board[1]) or (board[8] and board[7] and board[2])or
+	(board[0] and board[4] and board[8]) or (board[2] and board[4] and board[6])or
+	(board[8] and board[4] and board[0]) or (board[6] and board[4] and board[2])):
+	break
+	"""THIS IS FOR PLAYER 1"""
+	if ((board[0] and board[1] and board[2]) or (board[3] and board[4] and board[5]) or (board[6] and board[7] and board[8]) or
+	(board[2] and board[1] and board[0]) or (board[5] and board[4] and board[3]) or (board[8] and board[7] and board[6])or
+	(board[0] and board[3] and board[6]) or (board[1] and board[4] and board[7]) or (board[2] and board[5] and board[8]) or
+	(board[6] and board[3] and board[0]) or (board[7] and board[4] and board[1]) or (board[8] and board[7] and board[2])or
+	(board[0] and board[4] and board[8]) or (board[2] and board[4] and board[6])or
+	(board[8] and board[4] and board[0]) or (board[6] and board[4] and board[2])=='x'):
+	print("Player1 Wins")
+	else:
+	print("Player 2")
+	GAME()
+
